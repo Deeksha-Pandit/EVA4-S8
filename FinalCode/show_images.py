07b32ff8c36dac3cba465b8915c6ebfb75f042d2
Deeksha-Pandit <deekshapandit@gmail.com>
2020-03-15 00:21:56 +0530

    Add files via upload

---
+++ FinalCode/show_images.py
@@ -0,0 +1,22 @@
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import numpy as np
+
+# functions to show an image
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+def show_random_images(dataset, classes):
+    # get some random training images
+    dataiter = iter(dataset)
+    images, labels = dataiter.next()
+
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+    # print labels
+    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
